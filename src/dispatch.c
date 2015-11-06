834dbf63cdea899070e2ec158a5c2a5063e9348d
Gregor Riepl <onitake@gmail.com>
2015-11-06 18:09:15 +0100

    Reworked dispatch scheduler

--- src/dispatch.c
+++ src/dispatch.c
@@ -1,9 +1,9 @@
 /**
- * Matemat controller firmware
- * Copyright (C) 2014 Chaostreff Basel
+ * @file dispatch.c
+ * @brief Multi-priority dispatch queue
  * 
- * dispatch.c
- * Dispatch queue implementation
+ * @copyright Matemat controller firmware
+ * Copyright © 2014 Chaostreff Basel
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,55 +20,118 @@
  */
 
 #include <string.h>
+#include <avr/interrupt.h>
 #include <aversive/irq_lock.h>
 #include "dispatch.h"
 
-dispatch_t *dispatch_init(void *buffer, dispatch_size_t size) {
-	if (size >= sizeof(dispatch_t)) {
+dispatch_t *dispatch_init(void *buffer, size_t size, dispatch_priority_t queues, uint8_t *lengths) {
+	if (buffer) {
 		dispatch_t *dispatch = (dispatch_t *) buffer;
-		char *queue = ((char *) buffer) + sizeof(dispatch_t);
-		dispatch->length = (size - sizeof(dispatch_t)) / sizeof(event_t);
-		dispatch->used = 0;
-		cirbuf_init(&(dispatch->queue), queue, 0, dispatch->length * sizeof(event_t));
+		buffer = (uint8_t *) buffer + sizeof(dispatch_t);
+		size -= sizeof(dispatch_t);
+		dispatch->queues = queues;
+		dispatch->priority = 0;
+		dispatch->time = 0;
+		if (size >= sizeof(dispatch_head_t) * queues) {
+			dispatch->heads = (dispatch_head_t *) buffer;
+			buffer = (uint8_t *) buffer + sizeof(dispatch_head_t) * queues;
+			size -= sizeof(dispatch_head_t) * queues;
+			dispatch_priority_t priority;
+			for (priority = 0; priority < queues; priority++) {
+				dispatch_head_t *queue = &dispatch->heads[priority];
+				queue->length = lengths[priority];
+				queue->queued = 0;
+				queue->priority = priority;
+				dispatch_event_t *first = (dispatch_event_t *) buffer;
+				queue->in = first;
+				queue->out = first;
+				dispatch_event_t *prev = first;
+				uint8_t length;
+				for (length = 0; length < queue->length; length++) {
+					if (size < sizeof(dispatch_event_t)) {
+						return NULL;
+					}
+					buffer = (uint8_t *) buffer + sizeof(dispatch_event_t);
+					size -= sizeof(dispatch_event_t);
+					prev->priority = priority;
+					if (length == queue->length - 1) {
+						prev->next = first;
+					} else {
+						dispatch_event_t *next = (dispatch_event_t *) buffer;
+						prev->next = next;
+						prev = next;
+					}
+				}
+			}
+			return dispatch;
+		}
 	}
 	return NULL;
 }
 
 void dispatch_shutdown(dispatch_t *dispatch) {
-	// Do nothing
+	if (dispatch) {
+	}
 }
 
-dispatch_size_t dispatch_count(dispatch_t *dispatch) {
-	return dispatch->used;
+size_t dispatch_count(dispatch_t *dispatch, dispatch_priority_t priority) {
+	if (dispatch && priority < dispatch->queues) {
+		uint8_t flags;
+		IRQ_LOCK(flags);
+		size_t count = dispatch->heads[priority].queued;
+		IRQ_UNLOCK(flags);
+		return count;
+	}
+	return 0;
 }
 
-bool dispatch_dequeue(dispatch_t *dispatch, event_t *event) {
-	uint8_t flags;
-	IRQ_LOCK(flags);
-	bool ok = dispatch->used > 0;
-	if (ok) {
-		ok = cirbuf_get_buf_head(&(dispatch->queue), (char *) event, sizeof(event_t)) == sizeof(event_t);
-		if (ok) {
-			ok = cirbuf_del_buf_head(&(dispatch->queue), sizeof(event_t)) == 0;
-			if (ok) {
-				dispatch->used--;
+void dispatch_tick(dispatch_t *dispatch) {
+	if (dispatch) {
+		dispatch->time++;
+		bool handled = false;
+		dispatch_priority_t priority;
+		for (priority = dispatch->queues - 1; !handled && priority > dispatch->priority; priority--) {
+			if (dispatch->heads[priority].queued > 0) {
+				dispatch_priority_t old = dispatch->priority;
+				dispatch->priority = priority;
+				
+				dispatch->priority = old;
+				handled = true;
 			}
 		}
+		if (!handled) {
+		}
+		// TODO check if any queue of higher priority than dispatch->priority has events waiting
+		// set dispatch->priority to the priority of that queue
+		// unqueue the first event
+		// reenable interrupts
+		// call the event handler
+		// release the event object memory
 	}
-	IRQ_UNLOCK(flags);
-	return ok;
 }
 
-bool dispatch_enqueue(dispatch_t *dispatch, const event_t *event) {
-	uint8_t flags;
-	IRQ_LOCK(flags);
-	bool ok = dispatch->used < dispatch->length;
-	if (ok) {
-		ok = cirbuf_add_buf_tail(&(dispatch->queue), (const char *) event, sizeof(event_t)) == sizeof(event_t);
-		if (ok) {
-			dispatch->used++;
+bool dispatch_schedule(dispatch_t *dispatch, dispatch_handler_t *handler, void *argument, dispatch_priority_t priority) {
+	return dispatch_schedule_in(dispatch, handler, argument, priority, 0);
+}
+
+bool dispatch_schedule_in(dispatch_t *dispatch, dispatch_handler_t *handler, void *argument, dispatch_priority_t priority, dispatch_time_t deadline) {
+	return dispatch_schedule_at(dispatch, handler, argument, priority, dispatch->time + deadline);
+}
+
+bool dispatch_schedule_at(dispatch_t *dispatch, dispatch_handler_t *handler, void *argument, dispatch_priority_t priority, dispatch_time_t wallclock) {
+	if (dispatch && priority < dispatch->queues) {
+		uint8_t flags;
+		IRQ_LOCK(flags);
+		dispatch_head_t *queue = &dispatch->heads[priority];
+		if (queue->queued < queue->length) {
+			dispatch_event_t *event = queue->in;
+			queue->in = queue->in->next;
+			event->handler = handler;
+			event->argument = argument;
+			event->deadline = wallclock;
 		}
+		IRQ_UNLOCK(flags);
+		return true;
 	}
-	IRQ_UNLOCK(flags);
-	return ok;
+	return false;
 }
--- src/dispatch.h
+++ src/dispatch.h
@@ -1,9 +1,29 @@
 /**
- * Matemat controller firmware
- * Copyright (C) 2014 Chaostreff Basel
+ * @file dispatch.h
+ * @brief Multi-priority dispatch queue
  * 
- * dispatch.h
- * Dispatch queue interface
+ * Each priority level has its own queue for a fixed number of events;
+ * the queue length is configurable.
+ * 
+ * The queue relies on a storage pool that needs to be specified on creation.
+ * 
+ * Memory pool layout:
+ * | offset | usage           |
+ * |--------|-----------------|
+ * | 0      | dispatch        |
+ * | 6      | queue head 0    |
+ * | 15     | queue head 1    |
+ * | 24     | ...             |
+ * | N      | queue 0 event 0 |
+ * | N+9    | queue 0 event 1 |
+ * | N+18   | queue 0 event 2 |
+ * | N+27   | ...             |
+ * | M      | queue 1 event 0 |
+ * | M+9    | queue 1 event 1 |
+ * | M+18   | ...             |
+ * 
+ * @copyright Matemat controller firmware
+ * Copyright © 2014 Chaostreff Basel
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,72 +42,187 @@
 #ifndef _DISPATCH_H
 #define _DISPATCH_H
 
+#include <stdlib.h>
 #include <stdint.h>
 #include <stdbool.h>
-#include <base/cirbuf/cirbuf.h>
-#include "event.h"
+#include "memory.h"
 
 /**
- * Storage size type for dispatch queues
+ * Priority level
  */
-typedef uint16_t dispatch_size_t;
+typedef uint8_t dispatch_priority_t;
 
 /**
- * Data structure holding dispatch queue metadata
+ * Wall clock time type
  */
-struct dispatch_t {
-	dispatch_size_t length;
-	dispatch_size_t used;
-	struct cirbuf queue;
-	// char queue[]
-};
-typedef struct dispatch_t dispatch_t;
+typedef uint16_t dispatch_time_t;
 
 /**
- * Calculates the minimum required memory pool size for a given number of queueable events
+ * Event handler callback type.
+ * 
+ * Example callback function declaration:
+ * ~~~{.c}
+ * void handler(void *event) {
+ *     if (event) {
+ *         type_t *priv = (type_t *) event;
+ *         ...
+ *     }
+ * }
+ * ~~~
  */
-#define DISPATCH_POOL_SIZE(events) (sizeof(dispatch_t) + sizeof(event_t) * events)
+typedef void (dispatch_handler_t)(void *event);
 
 /**
- * Initialises a dispatch queue.
- * @param buffer a storage pool, will be used for both metadata and the data pool itself
+ * Single event
+ * 
+ * All events are chained in a circular fashion. The last event points to
+ * the first.
+ */
+typedef struct dispatch_event_t {
+	/** Pointer to next event */
+	struct dispatch_event_t *next;
+	/** Event handler */
+	dispatch_handler_t *handler;
+	/** Event handler argument */
+	void *argument;
+	/** Priority level to be set when the handler is called */
+	dispatch_priority_t priority;
+	/** When this event must be scheduled */
+	dispatch_time_t deadline;
+} dispatch_event_t;
+
+/**
+ * Queue header
+ */
+typedef struct {
+	/** Length of this queue */
+	size_t length;
+	/** Number of queued events */
+	size_t queued;
+	/** Priority level of this queue */
+	dispatch_priority_t priority;
+	/** Pointer to first free event */
+	dispatch_event_t *in;
+	/** Pointer to first allocated event */
+	dispatch_event_t *out;
+} dispatch_head_t;
+
+/**
+ * Multi-priority dispatch queue/scheduler.
+ */
+typedef struct {
+	/** Number of queues/priority levels */
+	dispatch_priority_t queues;
+	/** Current wallclock time */
+	dispatch_time_t time;
+	/** Current priority level */
+	dispatch_priority_t priority;
+	/** Pointer to array of queue headers */
+	dispatch_head_t *heads;
+} dispatch_t;
+
+/**
+ * Calculates the required memory pool size for a maximum priority level
+ * and a given number of queueable events, distributed over all priorities.
+ * @param priorities the number of priority levels
+ * @param events the total number of queueable events
+ * @return the number of bytes required for the dispatch queue storage pool
+ */
+#define DISPATCH_POOL_SIZE(priorities, events) (sizeof(dispatch_t) + sizeof(dispatch_head_t) * priorities + sizeof(dispatch_event_t) * events)
+
+/**
+ * Creates and initialises a dispatch queue.
+ * All storage will be drawn from the pool, even the dispatch object itself.
+ * Make sure the buffer is large enough. You can use
+ * DISPATCH_POOL_SIZE to allocate a suitable buffer.
+ * After creating the dispatch queue, call dispatch_schedule() in regular
+ * intervals (preferably from a timer interrupt).
+ * @param buffer a storage pool, used for the event queues and private event data
  * @param size the size of the storage pool, in bytes
- * @return a pointer to a dispatch queue object, or NULL if the buffer was too small to hold metadata
+ * @param queues the number of priority levels
+ * @param lengths an array containing the queue depth for each priority level
+ * @return a pointer to a newly initialized dispatch queue
  */
-dispatch_t *dispatch_init(void *buffer, dispatch_size_t size);
+dispatch_t *dispatch_init(void *buffer, size_t size, dispatch_priority_t queues, uint8_t *lengths);
 /**
  * Destroys a dispatch queue.
  * External resources are not released by this function. For example, if you
  * used dynamic memory for the storage pool, you need to free it yourself.
+ * The system timer will be stopped if it is running.
+ * The interrupt flag is not modified.
  * @param dispatch the dispatch queue to destroy
  */
 void dispatch_shutdown(dispatch_t *dispatch);
 
 /**
- * Checks the number of queued events on a dispatch queue.
- * Note that concurrent modifications are possible after this method returns.
- * @param dispatch the dispatch queue to query
- * @return the number of events currently waiting
+ * Periodic scheduler callback.
+ * Typically, call this from a system timer interrupt.
+ * When the function is entered, interrupts should be disabled
+ * (which is the case if called from an ISR).
+ * Events are scheduled strictly by priority, there is no preemption if no
+ * events of higher priority are waiting. Interrupts are reenabled before the
+ * event handler is called.
+ * After the handler returns, the event object is released automatically.
+ * @param dispatch the dispatch queue being updated
  */
-dispatch_size_t dispatch_count(dispatch_t *dispatch);
+void dispatch_tick(dispatch_t *dispatch);
 
 /**
- * Atomically fetches an event from a dispatch queue and returns it.
- * Call dispatch_release after you are done processing the event. You may
- * enqueue additional events while the event object is still in use, but be
- * aware that it still occupies space in the data pool.
- * @param dispatch the dispatch queue to access
- * @param event an event buffer to copy the dequeued event into
- * @return true, if an event was dequeued, false if the queue was empty
+ * Checks the number of queued events on a dispatch queue.
+ * 
+ * Please take concurrent modifications of the queue into account when
+ * calling this method.
+ * 
+ * @param dispatch the dispatch queue to query
+ * @param priority the priority level to count
+ * @return the number of events currently waiting
  */
-bool dispatch_dequeue(dispatch_t *dispatch, event_t *event);
+size_t dispatch_count(dispatch_t *dispatch, dispatch_priority_t priority);
+
 /**
- * Atomically enqueues an event.
- * All data from event is copied into the pool.
- * @param dispatch the dispatch queue to access
- * @param event the event to put on the queue
- * @return true, if the event was queued successfully; false, if the queue is full
+ * Gets the current wall clock time of the timer used for scheduling events.
+ * @return the current time in ticks
  */
-bool dispatch_enqueue(dispatch_t *dispatch, const event_t *event);
+dispatch_time_t dispatch_time(dispatch_t *dispatch);
+
+/**
+ * Schedules an event for execution.
+ * 
+ * The handler will be executed as soon as all events with a higher priority
+ * have been processed and no previous events of the same priority are waiting.
+ * 
+ * @param dispatch the dispatch queue to schedule on
+ * @param handler the handler callback for the event
+ * @param event a private event argument, or NULL
+ * @param priority the priority level of this thread
+ * @return true, if the event was scheduled successfully
+ */
+bool dispatch_schedule(dispatch_t *dispatch, dispatch_handler_t *handler, void *event, dispatch_priority_t priority);
+/**
+ * Schedules an event for execution, with a delay.
+ * The handler will be executed as soon as all events with a higher priority
+ * have been processed, no previous events of the same priority are waiting and
+ * the deadline has elapsed.
+ * @param dispatch the dispatch queue to schedule on
+ * @param handler the handler callback for the event
+ * @param event a private event argument, or NULL
+ * @param priority the priority level of this thread
+ * @param deadline the minimum number of timer ticks to wait before executing the event
+ * @return true, if the event was scheduled successfully
+ */
+bool dispatch_schedule_in(dispatch_t *dispatch, dispatch_handler_t *handler, void *event, dispatch_priority_t priority, dispatch_time_t deadline);
+/**
+ * Schedules an event for execution, with a delay.
+ * The handler will be executed as soon as all events with a higher priority
+ * have been processed, no previous events of the same priority are waiting and
+ * the deadline has elapsed.
+ * @param dispatch the dispatch queue to schedule on
+ * @param handler the handler callback for the event
+ * @param event a private event argument, or NULL
+ * @param priority the priority level of this thread
+ * @param wallclock the exact wall clock time when to execute the event (may be delayed due to prioritised events)
+ * @return true, if the event was scheduled successfully
+ */
+bool dispatch_schedule_at(dispatch_t *dispatch, dispatch_handler_t *handler, void *event, dispatch_priority_t priority, dispatch_time_t wallclock);
 
 #endif /*_DISPATCH_H*/
