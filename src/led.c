e36c3b0012ddfe0230adba7e3fa9096474787306
Gregor Riepl <onitake@gmail.com>
2015-11-06 18:40:14 +0100

    Updated LED driver with callout support

--- src/led.c
+++ src/led.c
@@ -1,9 +1,9 @@
 /**
- * Matemat controller firmware
- * Copyright (C) 2014 Chaostreff Basel
+ * @file led.c
+ * @brief LED port driver implementation
  * 
- * led.c
- * LED port driver implementation
+ * @copyright Matemat controller firmware
+ * Copyright © 2014 Chaostreff Basel
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,32 +20,118 @@
  */
 
 #include <avr/io.h>
+#include <avr/interrupt.h>
+#include <aversive/irq_lock.h>
 #include "led.h"
-#include "dispatch.h"
+#include "memory.h"
 
+/* TODO These should go into a configuration header */
+
+/** LED A bit index */
 #define LED_P_A PG0
+/** LED A port register */
 #define LED_PORT_A PORTG
+/** LED A pin register */
 #define LED_PIN_A PING
+/** LED A direction register */
 #define LED_DDR_A DDRG
+/** LED B bit index */
 #define LED_P_B PG1
+/** LED B port register */
 #define LED_PORT_B PORTG
+/** LED B pin register */
 #define LED_PIN_B PING
+/** LED B direction register */
 #define LED_DDR_B DDRG
+/** LED C bit index */
 #define LED_P_C PG2
+/** LED C port register */
 #define LED_PORT_C PORTG
+/** LED C pin register */
 #define LED_PIN_C PING
+/** LED C direction register */
 #define LED_DDR_C DDRG
 
-void led_init() {
-	LED_PORT_A &= ~_BV(LED_P_A);
-	LED_DDR_A |= _BV(LED_P_A);
-	LED_PORT_B &= ~_BV(LED_P_B);
-	LED_DDR_B |= _BV(LED_P_B);
-	LED_PORT_C &= ~_BV(LED_P_C);
-	LED_DDR_C |= _BV(LED_P_C);
+/**
+ * LED driver event structure
+ */
+typedef struct {
+	/** Event data (self-referenced) */
+	struct callout co;
+	/** Event type */
+	led_event_type_e type;
+	/** Affected LED */
+	led_name_e name;
+	/** On time */
+	uint16_t on;
+	/** Off time */
+	uint16_t off;
+	/** Blink? */
+	bool blink;
+	/** Periodic? */
+	bool periodic;
+} led_event_t;
+
+/**
+ * LED driver object
+ */
+typedef struct {
+	/** Event queue */
+	struct callout_mgr *manager;
+	/** Preallocated events for blink requests */
+	led_event_t blink[3];
+	/** Memory manager for the event queue */
+	memory_t *memory;
+	/** Managed memory pool */
+	uint8_t pool[MEMORY_POOL_SIZE(LED_QUEUE_SIZE, sizeof(led_event_t))];
+} led_t;
+
+/**
+ * Global driver object
+ */
+static led_t led_global __attribute__((section(".noinit")));
+
+/**
+ * Handle a LED event.
+ * @param cm the event queue manager
+ * @param tim the event information
+ * @param arg a private argument
+ */
+static void led_callback(struct callout_mgr *cm, struct callout *tim, void *arg);
+
+bool led_init(struct callout_mgr *manager) {
+	led_global.memory = memory_init(led_global.pool, sizeof(led_global.pool), sizeof(led_event_t));
+
+	led_name_e led;
+	for (led = 0; led < LED_MAX; led++) {
+		led_event_t *event = &led_global.blink[led];
+		event->name = led;
+		event->blink = true;
+		callout_init(&event->co, led_callback, event, LED_PRIORITY);
+	}
+	
+	if (led_global.memory) {
+		led_global.manager = manager;
+		
+		LED_PORT_A &= ~_BV(LED_P_A);
+		LED_DDR_A |= _BV(LED_P_A);
+		LED_PORT_B &= ~_BV(LED_P_B);
+		LED_DDR_B |= _BV(LED_P_B);
+		LED_PORT_C &= ~_BV(LED_P_C);
+		LED_DDR_C |= _BV(LED_P_C);
+		
+		return true;
+	}
+
+	return false;
 }
 
 void led_shutdown(bool off) {
+	led_name_e led;
+	for (led = 0; led < LED_MAX; led++) {
+		callout_stop(led_global.manager, &led_global.blink[led].co);
+	}
+	
 	if (off) {
 		LED_PORT_A &= ~_BV(LED_P_A);
 		LED_DDR_A &= ~_BV(LED_P_A);
@@ -56,68 +142,120 @@ void led_shutdown(bool off) {
 	}
 }
 
-bool led_send(dispatch_t *dispatch, event_target_e source, uint8_t led, uint8_t action) {
-	event_t event;
-	led_event_t *priv = EVENT_PRIVATE(&event, led_event_t);
-	event.source = source;
-	event.destination = EVENT_TARGET_LED;
-	priv->type = action;
-	priv->name = led;
-	return dispatch_enqueue(dispatch, &event);
+bool led_action(led_name_e led, led_event_type_e action) {
+	if (led < LED_MAX) {
+		uint8_t flags;
+		IRQ_LOCK(flags);
+		led_event_t *event = (led_event_t *) memory_allocate(led_global.memory);
+		IRQ_UNLOCK(flags);
+		
+		if (event) {
+			event->type = action;
+			event->name = led;
+			event->blink = false;
+			
+			callout_init(&event->co, led_callback, event, LED_PRIORITY);
+			
+			return callout_schedule(led_global.manager, &event->co, 0) == 0;
+		}
+	}
+	return false;
 }
 
-bool led_handle(event_t *event) {
-	led_event_t *priv = EVENT_PRIVATE(event, led_event_t);
-	bool handled = false;
-	switch (priv->type) {
-		case LED_EVENT_TYPE_ON:
-			switch (priv->name) {
-				case 0:
-					LED_PORT_A |= _BV(LED_P_A);
-					handled = true;
-					break;
-				case 1:
-					LED_PORT_B |= _BV(LED_P_B);
-					handled = true;
-					break;
-				case 2:
-					LED_PORT_C |= _BV(LED_P_C);
-					handled = true;
-					break;
-			}
-			break;
-		case LED_EVENT_TYPE_OFF:
-			switch (priv->name) {
-				case 0:
-					LED_PORT_A &= ~_BV(LED_P_A);
-					handled = true;
-					break;
-				case 1:
-					LED_PORT_B &= ~_BV(LED_P_B);
-					handled = true;
-					break;
-				case 2:
-					LED_PORT_C &= ~_BV(LED_P_C);
-					handled = true;
-					break;
-			}
-			break;
-		case LED_EVENT_TYPE_TOGGLE:
-			switch (priv->name) {
-				case 0:
-					LED_PORT_A ^= _BV(LED_P_A);
-					handled = true;
-					break;
-				case 1:
-					LED_PORT_B ^= _BV(LED_P_B);
-					handled = true;
-					break;
-				case 2:
-					LED_PORT_C ^= _BV(LED_P_C);
-					handled = true;
-					break;
-			}
-			break;
+bool led_blink(led_name_e led, uint16_t ontime, uint16_t offtime, bool repeat) {
+	if (led < LED_MAX) {
+		led_event_t *event = &led_global.blink[led];
+		
+		callout_stop(led_global.manager, &event->co);
+	
+		event->type = LED_EVENT_TYPE_ON;
+		event->on = ontime;
+		event->off = offtime;
+		event->periodic = repeat;
+
+		// TODO use callout.now instead of 0
+		return callout_schedule(led_global.manager, &event->co, 0) == 0;
 	}
-	return handled;
+	return false;
+}
+
+static void led_callback(struct callout_mgr *cm, struct callout *tim, void *arg) {
+	uint8_t flags;
+	if (arg) {
+		led_event_t *priv = (led_event_t *) arg;
+		
+		switch (priv->type) {
+			case LED_EVENT_TYPE_ON:
+				switch (priv->name) {
+					case 0:
+						LED_PORT_A |= _BV(LED_P_A);
+						break;
+					case 1:
+						LED_PORT_B |= _BV(LED_P_B);
+						break;
+					case 2:
+						LED_PORT_C |= _BV(LED_P_C);
+						break;
+					default:
+						break;
+				}
+				break;
+			case LED_EVENT_TYPE_OFF:
+				switch (priv->name) {
+					case 0:
+						LED_PORT_A &= ~_BV(LED_P_A);
+						break;
+					case 1:
+						LED_PORT_B &= ~_BV(LED_P_B);
+						break;
+					case 2:
+						LED_PORT_C &= ~_BV(LED_P_C);
+						break;
+					default:
+						break;
+				}
+				break;
+			case LED_EVENT_TYPE_TOGGLE:
+				switch (priv->name) {
+					case 0:
+						LED_PORT_A ^= _BV(LED_P_A);
+						break;
+					case 1:
+						LED_PORT_B ^= _BV(LED_P_B);
+						break;
+					case 2:
+						LED_PORT_C ^= _BV(LED_P_C);
+						break;
+					default:
+						break;
+				}
+				break;
+		}
+		
+		if (priv->blink) {
+			if (priv->type == LED_EVENT_TYPE_ON) {
+				priv->type = LED_EVENT_TYPE_OFF;
+				callout_init(&priv->co, led_callback, priv, LED_PRIORITY);
+				// FIXME callout_reschedule does not seem to do what we expect it to do, so we use callout_schedule instead.
+				if (callout_schedule(led_global.manager, &priv->co, priv->on) == 0) {
+					return;
+				}
+			} else {
+				if (priv->periodic) {
+					priv->type = LED_EVENT_TYPE_ON;
+					callout_init(&priv->co, led_callback, priv, LED_PRIORITY);
+					// FIXME callout_reschedule does not seem to do what we expect it to do, so we use callout_schedule instead.
+					if (callout_schedule(led_global.manager, &priv->co, priv->off) == 0) {
+						return;
+					}
+				}
+			}
+		} else {
+			callout_stop(led_global.manager, &led_global.blink[priv->name].co);
+		}
+		
+		IRQ_LOCK(flags);
+		memory_release(arg);
+		IRQ_UNLOCK(flags);
+	}
 }
--- src/led.h
+++ src/led.h
@@ -1,9 +1,23 @@
 /**
- * Matemat controller firmware
- * Copyright (C) 2014 Chaostreff Basel
+ * @file led.h
+ * @brief LED port driver interface
  * 
- * led.h
- * LED port driver interface
+ * The LED driver supports periodic and one-shot actions. The event queue
+ * is used for all of them
+ * 
+ * led_action() turns a LED on or off, or toggles its state.
+ * led_blink() can be used to turn a LED on and off again after a delay,
+ * or to make it blink periodically.
+ * 
+ * @par Configurable options
+ * 
+ * Macro               | Default  | Values         | Description
+ * --------------------|----------|----------------|-----------------------------------------------
+ * LED_QUEUE_SIZE      | [undef]  | 0..255         | Size of the event pool
+ * LED_PRIORITY        | [undef]  | 0..127         | Event queue priority
+ * 
+ * @copyright Matemat controller firmware
+ * Copyright © 2014 Chaostreff Basel
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -22,62 +36,65 @@
 #ifndef _LED_H
 #define _LED_H
 
-#include "event.h"
+#include <stdbool.h>
+#include <base/callout/callout.h>
 
 /**
  * LED driver event types
  */
-enum led_event_type_e {
+typedef enum {
+	/** Turn LED on */
 	LED_EVENT_TYPE_ON,
+	/** Turn LED off */
 	LED_EVENT_TYPE_OFF,
+	/** Flip LED state */
 	LED_EVENT_TYPE_TOGGLE,
-};
-typedef enum led_event_type_e led_event_type_e;
+} led_event_type_e;
 
 /**
  * LED identifiers
  */
-enum led_name_e {
-	LED_A,
+typedef enum {
+	/** LED A */
+	LED_A = 0,
+	/** LED B */
 	LED_B,
+	/** LED C */
 	LED_C,
-};
-typedef enum led_name_e led_name_e;
+	/** Number of LEDs */
+	LED_MAX,
+} led_name_e;
 
 /**
- * LED process event structure
+ * Initialise the (global) LED driver.
+ * @param manager the callout queue to use for passing events
+ * @return true, if initialisation was successful
  */
-struct led_event_t {
-	led_event_type_e type;
-	led_name_e name;
-};
-typedef struct led_event_t led_event_t;
-EVENT_SIZE_CHECK(struct led_event_t);
+bool led_init(struct callout_mgr *manager);
 
 /**
- * Initialises the LED driver.
- */
-void led_init(void);
-/**
- * Shuts the LED driver down.
- * @param off If true, also turns the LEDs off before shutdown.
+ * Shut the LED driver down.
+ * @param off if true, also turns all LEDs off before shutdown
  */
 void led_shutdown(bool off);
 
 /**
- * Send a LED event.
- * @param source The module sending the event.
- * @param led The LED to act upon (LED_A, LED_B or LED_C).
- * @param action The action to take (LED_EVENT_TYPE_ON, LED_EVENT_TYPE_OFF or LED_EVENT_TYPE_TOGGLE)
+ * Send a command to the LED driver.
+ * @param led the LED to act upon
+ * @param action the action to take (LED_EVENT_TYPE_OFF, LED_EVENT_TYPE_ON or
+ * LED_EVENT_TYPE_TOGGLE)
  * @return true, if the event was queued successfully.
  */
-bool led_send(dispatch_t *dispatch, event_target_e source, uint8_t led, uint8_t action);
+bool led_action(led_name_e led, led_event_type_e action);
 
 /**
- * Handle a LED event.
- * @param event The event
- * @return true, if the event was handled successfully.
+ * Schedule a periodic or one-shot event to make the LED turn off and on
+ * @param led the LED to act upon
+ * @param ontime the length of the on-time in ticks
+ * @param offtime the length of the off-time in ticks
+ * @param repeat true, if the blinking should be periodic
+ * @return true, if the event was queued successfully.
  */
-bool led_handle(event_t *event);
+bool led_blink(led_name_e led, uint16_t ontime, uint16_t offtime, bool repeat);
 
 #endif /*_LED_H*/
