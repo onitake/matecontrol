25dfc9d987e985bd288e520ac43fc57fbcb80f67
Gregor Riepl <onitake@gmail.com>
2015-12-17 19:49:00 +0100

    Changed return type of basic difftime implementation to match implementation

--- src/clock.c
+++ src/clock.c
@@ -97,8 +97,8 @@ void set_system_time(time_t timestamp) {
 	IRQ_UNLOCK(flags);
 }
 
-int32_t difftime(time_t time1, time_t time0) {
-	return (int32_t) time1 - time0;
+time_t difftime(time_t time1, time_t time0) {
+	return time1 - time0;
 }
 
 #endif /*HAVE_TIME_H*/
